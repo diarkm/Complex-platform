a38dece2027198bd5472d3ae4e61b8a6620fa7ad
muccurly <66302816+muccurly@users.noreply.github.com>
2021-04-03 16:20:38 +0600

    3,2 *

--- src/components/@vuexy/statisticsCard/UserCards.js
+++ src/components/@vuexy/statisticsCard/UserCards.js
@@ -10,8 +10,8 @@ class UserCards extends React.Component {
           className={`${this.props.className ? this.props.className : "stats-card-body"} d-flex justify-content-left align-items-center
            ${!this.props.hideChart ? "pb-0" : "pb-2"} pt-2`}
         >
-        <div className={`avatar mr-2`}>
-          {this.props.icon ? <img width='45' height='45' src={this.props.icon} /> : <Skeleton circle={true} width={45} height={45}/>}
+        <div className={`avatar mr-2`} style={{height:45,width:45,overflow:"hidden"}}>
+          {this.props.icon ? <img style={{borderRadius:0}} src={this.props.icon} /> : <Skeleton circle={true} width={45} height={45}/>}
             
         </div>
         <div className="title-section">
