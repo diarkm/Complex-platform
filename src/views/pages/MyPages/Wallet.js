74427b43f8af4012bdab3ee01deb8df694f02da1
Diar Kundakbayev <kundakbaev.d@gmail.com>
2021-05-17 12:30:21 +0600

    PWA

--- src/views/pages/MyPages/Wallet.js
+++ src/views/pages/MyPages/Wallet.js
@@ -1,12 +1,10 @@
 import React from "react";
-import {
-  Row,
-  Col,
-} from "reactstrap";
+import { Row, Col, Card, CardHeader } from "reactstrap";
 import FundsWallet from "../../ui-elements/cards/analytics/FundsWallet";
 import SuberscribersGained from "../../ui-elements/cards/statistics/SubscriberGained";
 import TreeView from "../../../extensions/treeview/TreeView";
 import DataTableOrders from "../../tables/data-tables/DataTableOrders";
+import { FormattedMessage } from 'react-intl'
 
 class Wallet extends React.Component {
   render() {
@@ -19,6 +17,20 @@ class Wallet extends React.Component {
           <Col lg="8" md="8" sm="12">
             <DataTableOrders />
           </Col>
+        </Row>
+        <Row>
+          <Card className="w-100 p-2">
+            <h3></h3>
+            <CardHeader className="mx-auto flex-column">
+              <h4>
+                <FormattedMessage id="Снятие средств" />
+              </h4>
+            </CardHeader>
+            <p style={{textAlign: 'center'}}>
+            <FormattedMessage id="Для снятия средств, пожалуйста обратитесь к администратору"/>
+              <a href="mailto:giqgroup2021@gmail.com"> giqgroup2021@gmail.com</a>
+            </p>
+          </Card>
         </Row>
         <Row>
           <Col className="match-height" lg="8" md="8" sm="12">
